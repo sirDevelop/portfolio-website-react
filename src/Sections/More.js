c11437abaa050baae531089aea6e8c0550c4ea0c
Chen W <50025994+sirDevelop@users.noreply.github.com>
2024-10-23 23:08:10 -0700

    move resume

--- src/Sections/More.js
+++ src/Sections/More.js
@@ -118,7 +118,7 @@ const More = () => {
 								<a
 									className="ms-2"
 									target="_blank"
-									href={`${process.env.PUBLIC_URL}/resume.pdf`}
+									href={'/resume.pdf'}
 								>
 									Resume
 								</a>
